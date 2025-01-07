a4e5da199f2516210d61ec1acbcb5d9b85e90653
KDevWeb <99359640+AtKDevWeb@users.noreply.github.com>
2025-01-07 11:15:25 +0100

    Init. CRUD Branch

--- hub/src/main/java/lol/fmg/hub/controllers/usersControllers/UserController.java
+++ hub/src/main/java/lol/fmg/hub/controllers/usersControllers/UserController.java
@@ -2,6 +2,7 @@ package lol.fmg.hub.controllers.usersControllers;
 
 
 import lol.fmg.hub.models.users.User;
+import lol.fmg.hub.repositories.usersRepositories.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,25 +12,36 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
-
-
+    private final UserRepository userRepository;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     //CRUD
     //Create
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
     //ReadAll
     @GetMapping
     public ResponseEntity<List<User>> getUsers() {
+        List<User> users = userRepository.findAll();
+        if (users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
         return ResponseEntity.ok(users);
     }
-    //ReadOne
-    @GetMapping
+    //ReadOnebyId
+    @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(user);
     }
+
     //Updated
     @PutMapping
     public ResponseEntity<User> updateUser( @PathVariable Long id, @RequestBody User user) {
@@ -38,7 +50,7 @@ public class UserController {
     //Delete
     @DeleteMapping
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        if (.) {
+        if () {
             return ResponseEntity.noContent().build();
         }else {
             return ResponseEntity.notFound().build();
