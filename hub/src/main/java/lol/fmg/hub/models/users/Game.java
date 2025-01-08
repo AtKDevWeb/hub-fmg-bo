9c6f556886182616908d771da9000345f64c9b07
KDevWeb <99359640+AtKDevWeb@users.noreply.github.com>
2025-01-08 09:24:30 +0100

    Basic Game Model-Repository-Controller Ready

--- hub/src/main/java/lol/fmg/hub/models/users/Game.java
+++ hub/src/main/java/lol/fmg/hub/models/users/Game.java
@@ -14,13 +14,77 @@ public class Game {
     @Column(columnDefinition = "TEXT")
     private String description;
     @Column
-    private String url_image;
+    private String urlImage;
     @Column
-    private String url_video;
+    private String urlVideo;
     @Column
     private String genres;
     @Column
-    private String palteformes;
+    private String platforms;
     @Column
     private String summary;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public String getUrlVideo() {
+        return urlVideo;
+    }
+
+    public void setUrlVideo(String urlVideo) {
+        this.urlVideo = urlVideo;
+    }
 }
--- hub/src/main/java/lol/fmg/hub/repositories/usersRepositories/GameRepository.java
+++ hub/src/main/java/lol/fmg/hub/repositories/usersRepositories/GameRepository.java
@@ -3,6 +3,10 @@ package lol.fmg.hub.repositories.usersRepositories;
 import lol.fmg.hub.models.users.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameRepository extends JpaRepository<Game, Long> {
+import java.util.List;
 
+public interface GameRepository extends JpaRepository<Game, Long> {
+    List<Game> findByTitle(String title);
+    List<Game> findByDescriptionContainingIgnoreCase(String terms);
 }
+
