6d478e0506dc49fac7dd86f32e7ae561bbbb9156
KDevWeb <99359640+AtKDevWeb@users.noreply.github.com>
2025-03-06 18:53:56 +0100

    Correction of declaration in Entites of DB 3

--- hub/src/main/java/lol/fmg/hub/models/users/UserEventParticipateId.java
+++ hub/src/main/java/lol/fmg/hub/models/users/UserEventParticipateId.java
@@ -4,6 +4,8 @@ package lol.fmg.hub.models.users;
 import lol.fmg.hub.models.events.Event;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 public class UserEventParticipateId implements Serializable {
 
--- hub/src/main/java/lol/fmg/hub/models/users/UserSupportId.java
+++ hub/src/main/java/lol/fmg/hub/models/users/UserSupportId.java
@@ -1,6 +1,7 @@
 package lol.fmg.hub.models.users;
 
 import lombok.Data;
+import java.io.Serializable;
 
 @Data
 public class UserSupportId implements Serializable {
