fb22e0a1e2ce6aae206f8d8ec3db7333551e639b
KDevWeb <99359640+AtKDevWeb@users.noreply.github.com>
2025-03-12 00:48:52 +0100

    Testconsturuction des Repository

---
+++ hub/src/main/java/lol/fmg/hub/repositories/usersRepositories/GameRepository.java
@@ -0,0 +1,7 @@
+package lol.fmg.hub.repositories.usersRepositories;
+
+import lol.fmg.hub.models.users.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Integer> {
+}
